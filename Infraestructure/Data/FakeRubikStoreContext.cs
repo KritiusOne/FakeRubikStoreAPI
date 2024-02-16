801026a764519248bb5f4f4c0361b93948f86741
Julio Rodríguez <julio10489@gmail.com>
2024-02-16 14:25:51 -0500

    Changes

--- Infraestructure/Data/FakeRubikStoreContext.cs
+++ Infraestructure/Data/FakeRubikStoreContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Aplication.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infraestructure.Data;
@@ -15,189 +14,189 @@ public partial class FakeRubikStoreContext : DbContext
     {
     }
 
-    public virtual DbSet<Categoria> Categorias { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
 
-    public virtual DbSet<CategoriasProducto> CategoriasProductos { get; set; }
+    public virtual DbSet<ProductCategory> ProductCategories { get; set; }
 
-    public virtual DbSet<Direccion> Direccions { get; set; }
+    public virtual DbSet<UserDirection> Directions { get; set; }
 
-    public virtual DbSet<Envio> Envios { get; set; }
+    public virtual DbSet<Delivery> Deliveries { get; set; }
 
-    public virtual DbSet<Estado> Estados { get; set; }
+    public virtual DbSet<State> States { get; set; }
 
-    public virtual DbSet<Ordene> Ordenes { get; set; }
+    public virtual DbSet<Order> Orders { get; set; }
 
-    public virtual DbSet<Producto> Productos { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
 
-    public virtual DbSet<ProductosOrdene> ProductosOrdenes { get; set; }
+    public virtual DbSet<OrdersProducts> ProductsOrders { get; set; }
 
-    public virtual DbSet<ProductosProveedore> ProductosProveedores { get; set; }
+    public virtual DbSet<ProductsProviders> ProductsProviders { get; set; }
 
-    public virtual DbSet<Proveedore> Proveedores { get; set; }
+    public virtual DbSet<Provider> Providers { get; set; }
 
     public virtual DbSet<Review> Reviews { get; set; }
 
-    public virtual DbSet<Rol> Rols { get; set; }
+    public virtual DbSet<Role> Roles { get; set; }
 
-    public virtual DbSet<Usuario> Usuarios { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-519269S\\SQLEXPRESS;Database=FakeRubikStore;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
-
+    public virtual DbSet<User> Users { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Categoria>(entity =>
+        modelBuilder.Entity<Category>(entity =>
         {
-            entity.Property(e => e.Nombre)
+            entity.Property(e => e.Name)
                 .HasMaxLength(50)
-                .IsUnicode(false);
+                .IsUnicode(false).HasColumnName("Nombre");
         });
 
-        modelBuilder.Entity<CategoriasProducto>(entity =>
+        modelBuilder.Entity<ProductCategory>(entity =>
         {
             entity.ToTable("Categorias_Productos");
 
-            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.CategoriasProductos)
-                .HasForeignKey(d => d.IdCategoria)
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.ProductCategories)
+                .HasForeignKey(d => d.IdCategory)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Categorias_Productos_Categorias");
 
-            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.CategoriasProductos)
-                .HasForeignKey(d => d.IdProducto)
+            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.ProductCategories)
+                .HasForeignKey(d => d.IdProduct)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Categorias_Productos_Productos");
         });
 
-        modelBuilder.Entity<Direccion>(entity =>
+        modelBuilder.Entity<UserDirection>(entity =>
         {
             entity.ToTable("Direccion");
 
-            entity.Property(e => e.Ciudad)
+            entity.Property(e => e.City)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("ciudad");
-            entity.Property(e => e.Descripcion)
+            entity.Property(e => e.Description)
                 .HasMaxLength(100)
                 .IsUnicode(false)
                 .HasColumnName("descripcion");
-            entity.Property(e => e.Direccion1)
+            entity.Property(e => e.Address)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("Direccion");
-            entity.Property(e => e.Estado)
+            entity.Property(e => e.State)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("estado");
-            entity.Property(e => e.Pais)
+            entity.Property(e => e.Country)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("pais");
         });
 
-        modelBuilder.Entity<Envio>(entity =>
+        modelBuilder.Entity<Delivery>(entity =>
         {
-            entity.Property(e => e.Codigo)
+            entity.Property(e => e.Code)
                 .HasMaxLength(100)
                 .IsUnicode(false);
 
-            entity.HasOne(d => d.IdEstadoNavigation).WithMany(p => p.Envios)
-                .HasForeignKey(d => d.IdEstado)
+            entity.HasOne(d => d.IdStateNav).WithMany(p => p.Deliveries)
+                .HasForeignKey(d => d.IdState)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Envios_Estado");
 
-            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Envios)
-                .HasForeignKey(d => d.IdUsuario)
+            entity.HasOne(d => d.IdUserNav).WithMany(p => p.Deliveries)
+                .HasForeignKey(d => d.IdUser)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Envios_Usuarios");
         });
 
-        modelBuilder.Entity<Estado>(entity =>
+        modelBuilder.Entity<State>(entity =>
         {
-            entity.ToTable("Estado");
+            entity.ToTable("State");
 
-            entity.Property(e => e.Nombre)
+            entity.Property(e => e.Name)
                 .HasMaxLength(50)
-                .IsUnicode(false);
+                .IsUnicode(false).HasColumnName("Nombre");
         });
 
-        modelBuilder.Entity<Ordene>(entity =>
+        modelBuilder.Entity<Order>(entity =>
         {
-            entity.Property(e => e.Fecha).HasColumnType("datetime");
-
-            entity.HasOne(d => d.IdEnvioNavigation).WithMany(p => p.Ordenes)
-                .HasForeignKey(d => d.IdEnvio)
+            entity.Property(e => e.Date).HasColumnType("datetime").HasColumnName("Fecha");
+            entity.Property(e => e.FinalPrice).HasColumnName("PrecioFinal");
+            entity.Property(e => e.IdDelivery).HasColumnName("IdEnvio");
+            entity.HasOne(d => d.DeliveryNav).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.IdDelivery)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Ordenes_Envios");
 
-            entity.HasOne(d => d.IdUserNavigation).WithMany(p => p.Ordenes)
+            entity.HasOne(d => d.UserNav).WithMany(p => p.Orders)
                 .HasForeignKey(d => d.IdUser)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Ordenes_Usuarios");
         });
 
-        modelBuilder.Entity<Producto>(entity =>
+        modelBuilder.Entity<Product>(entity =>
         {
-            entity.Property(e => e.Descripcion)
+            entity.Property(e => e.Description)
                 .HasMaxLength(200)
-                .IsUnicode(false);
-            entity.Property(e => e.Imagen)
+                .IsUnicode(false).HasColumnName("Descripcion");
+            entity.Property(e => e.Image)
+                .HasColumnName("Imagen")
                 .HasMaxLength(50)
                 .IsUnicode(false);
-            entity.Property(e => e.Miniatura)
+            entity.Property(e => e.Thumbnail)
+                .HasColumnName("Miniatura")
                 .HasMaxLength(50)
                 .IsUnicode(false);
-            entity.Property(e => e.NombreProducto)
+            entity.Property(e => e.Name).HasColumnName("Nombre")
                 .HasMaxLength(50)
                 .IsUnicode(false);
 
-            entity.HasOne(d => d.IdReviewNavigation).WithMany(p => p.Productos)
+            entity.HasOne(d => d.ReviewNav).WithMany(p => p.Productos)
                 .HasForeignKey(d => d.IdReview)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Productos_Reviews");
         });
 
-        modelBuilder.Entity<ProductosOrdene>(entity =>
+        modelBuilder.Entity<OrdersProducts>(entity =>
         {
             entity.ToTable("Productos_Ordenes");
 
-            entity.HasOne(d => d.IdOrdenNavigation).WithMany(p => p.ProductosOrdenes)
-                .HasForeignKey(d => d.IdOrden)
+            entity.HasOne(d => d.OrderNav).WithMany(p => p.OrderProducts)
+                .HasForeignKey(d => d.IdOrder)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Productos_Ordenes_Ordenes");
 
-            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.ProductosOrdenes)
-                .HasForeignKey(d => d.IdProducto)
+            entity.HasOne(d => d.ProductNav).WithMany(p => p.OrderProducts)
+                .HasForeignKey(d => d.IdProduct)
                 .HasConstraintName("FK_Productos_Ordenes_Productos");
         });
 
-        modelBuilder.Entity<ProductosProveedore>(entity =>
+        modelBuilder.Entity<ProductsProviders>(entity =>
         {
             entity.ToTable("Productos_Proveedores");
 
-            entity.HasOne(d => d.IdProductosNavigation).WithMany(p => p.ProductosProveedores)
+            entity.HasOne(d => d.IdProductosNavigation).WithMany(p => p.ProvidersProducts)
                 .HasForeignKey(d => d.IdProductos)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Productos_Proveedores_Productos");
 
-            entity.HasOne(d => d.IdProveedoresNavigation).WithMany(p => p.ProductosProveedores)
+            entity.HasOne(d => d.IdProveedoresNavigation).WithMany(p => p.ProvidersProducts)
                 .HasForeignKey(d => d.IdProveedores)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Productos_Proveedores_Proveedores");
         });
 
-        modelBuilder.Entity<Proveedore>(entity =>
+        modelBuilder.Entity<Provider>(entity =>
         {
             entity.Property(e => e.Email)
                 .HasMaxLength(50)
                 .IsUnicode(false)
                 .HasColumnName("email");
-            entity.Property(e => e.Nombre)
+            entity.Property(e => e.Name)
                 .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.Telefono)
+                .IsUnicode(false)
+                .HasColumnName("Nombre");
+            entity.Property(e => e.Phone)
                 .HasMaxLength(30)
-                .IsUnicode(false);
+                .IsUnicode(false)
+                .HasColumnName("Telefono");
         });
 
         modelBuilder.Entity<Review>(entity =>
@@ -208,39 +207,43 @@ public partial class FakeRubikStoreContext : DbContext
             entity.Property(e => e.Rate).HasColumnName("rate");
         });
 
-        modelBuilder.Entity<Rol>(entity =>
+        modelBuilder.Entity<Role>(entity =>
         {
-            entity.HasKey(e => e.IdRol);
+            entity.HasKey(e => e.Id);
 
-            entity.ToTable("Rol");
-
-            entity.Property(e => e.Nombre)
+            entity.ToTable("Role");
+            entity.Property(e => e.Id).HasColumnName("IdRol");
+            entity.Property(e => e.Name)
                 .HasMaxLength(20)
-                .IsUnicode(false);
+                .IsUnicode(false)
+                .HasColumnName("Nombre");
         });
 
-        modelBuilder.Entity<Usuario>(entity =>
+        modelBuilder.Entity<User>(entity =>
         {
-            entity.Property(e => e.Apellido)
+            entity.Property(e => e.SecondName)
                 .HasMaxLength(50)
-                .IsUnicode(false);
+                .IsUnicode(false)
+                .HasColumnName("Apellido");
             entity.Property(e => e.Email)
                 .HasMaxLength(50)
                 .IsUnicode(false);
-            entity.Property(e => e.Nombre)
+            entity.Property(e => e.Name)
                 .HasMaxLength(50)
-                .IsUnicode(false);
-            entity.Property(e => e.Telefono)
+                .IsUnicode(false)
+                .HasColumnName("Nombre");
+            entity.Property(e => e.Phone)
                 .HasMaxLength(30)
-                .IsUnicode(false);
+                .IsUnicode(false)
+                .HasColumnName("Telefono");
 
-            entity.HasOne(d => d.IdDireccionNavigation).WithMany(p => p.Usuarios)
-                .HasForeignKey(d => d.IdDireccion)
+            entity.HasOne(d => d.UserDirectionNav).WithMany(p => p.Users)
+                .HasForeignKey(d => d.IdAddress)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Usuarios_Direccion");
 
-            entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.Usuarios)
-                .HasForeignKey(d => d.IdRol)
+            entity.HasOne(d => d.RoleNav).WithMany(p => p.Users)
+                .HasForeignKey(d => d.IdRole)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Usuarios_Rol");
         });
