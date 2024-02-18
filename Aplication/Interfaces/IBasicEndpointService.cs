72260840a2d5ae815b1dc09adf3d6d7e63ec69f8
Julio Rodríguez <julio10489@gmail.com>
2024-02-17 22:53:46 -0500

    Added: Add new Controller of the Management Estado Entity

---
+++ Aplication/Interfaces/IBasicEndpointService.cs
@@ -0,0 +1,10 @@
+﻿using Aplication.Entities;
+
+namespace Aplication.Interfaces
+{
+    public interface IBasicEndpointService<T> where T : BaseEntity //this not are ALL ENTITIES!!!!!
+    {
+        IEnumerable<T> GetAll();
+        Task Create(T entity);
+    }
+}
---
+++ Aplication/Services/BasicEndpointService.cs
@@ -0,0 +1,25 @@
+﻿using Aplication.Entities;
+using Aplication.Interfaces;
+
+namespace Aplication.Services
+{
+    public class BasicEndpointService<T> : IBasicEndpointService<T> where T : BaseEntity
+    {
+        private readonly IUnitOfWork<T> _unitOfWork;
+        public BasicEndpointService(IUnitOfWork<T> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;            
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _unitOfWork.BaseRepo.GetAll();
+        }
+        public async Task Create(T entity)
+        {
+            await _unitOfWork.BaseRepo.Add(entity);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+       
+    }
+}
