5b1f91beb64eb532897b765347c734b19b0d44d3
Julio Rodríguez <julio10489@gmail.com>
2024-02-17 22:53:46 -0500

    Added: Add new Controller of the Management Estado Entity

---
+++ API/Controllers/StateController.cs
@@ -0,0 +1,35 @@
+﻿using Aplication.DTOs;
+using Aplication.Entities;
+using Aplication.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        private readonly IBasicEndpointService<State> _basicEndpoint;
+        private readonly IMapper _mapper;
+        public StateController(IBasicEndpointService<State> basicEndpointService, IMapper mapper)
+        {
+            _basicEndpoint = basicEndpointService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var States = _basicEndpoint.GetAll();
+            return Ok(States);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(StateDTO stateDTO)
+        {
+            var state = _mapper.Map<State>(stateDTO);
+            await _basicEndpoint.Create(state);
+            return Ok();
+        }
+    }
+}
