8068577f9c9c29350ec0b508c8f4d70c94ba42a8
Julio Rodríguez <julio10489@gmail.com>
2024-02-19 10:49:47 -0500

    Added: Create archive UserException and GlobalExceptionFIlter for the personalizationed of the exception

---
+++ Infraestructure/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Aplication.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace Infraestructure.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if(filterContext.Exception.GetType() == typeof( UserException )) 
+            {
+                var exception = (UserException) filterContext.Exception;
+                var validation = new
+                {
+                    Status = 400,
+                    Title = "Bad Request",
+                    detail = exception.Message
+                };
+                var json = new
+                {
+                    errors = new[] { validation }
+                };
+                filterContext.Result = new BadRequestObjectResult(json);
+                filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
