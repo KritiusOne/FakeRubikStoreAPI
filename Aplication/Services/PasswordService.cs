110288e8aac9d8a32e47baf026d09436644d3a83
Julio Rodríguez <julio10489@gmail.com>
2024-02-20 20:41:51 -0500

    Added: Check method

--- Aplication/Services/PasswordService.cs
+++ Aplication/Services/PasswordService.cs
@@ -14,7 +14,25 @@ namespace Aplication.Services
 
         public bool Check(string hash, string password)
         {
-            throw new NotImplementedException();
+            var parts = hash.Split('.');
+            if (parts.Length != 3)
+            {
+                throw new FormatException("Unexpected hash format");
+            }
+
+            var iterations = Convert.ToInt32(parts[0]);
+            var salt = Convert.FromBase64String(parts[1]);
+            var key = Convert.FromBase64String(parts[2]);
+
+            using (var algorithm = new Rfc2898DeriveBytes(
+                password,
+                salt,
+                iterations
+                ))
+            {
+                var keyToCheck = algorithm.GetBytes(_options.KeySize);
+                return keyToCheck.SequenceEqual(key);
+            }
         }
 
         public string Hash(string password)
