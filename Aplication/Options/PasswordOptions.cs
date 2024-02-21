36ff31e8856f7e37cd440ed1c7a9c0d5ded15d1f
Julio Rodríguez <julio10489@gmail.com>
2024-02-20 20:16:37 -0500

    Added: Password hash

---
+++ Aplication/Options/PasswordOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Aplication.Options
+{
+    public class PasswordOptions
+    {
+        public int SaltSize = 16;
+        public int KeySize = 32;
+        public int Iterations = 10000;
+        public PasswordOptions()
+        {
+            
+        }
+    }
+}
---
+++ Aplication/Services/PasswordService.cs
@@ -0,0 +1,35 @@
+﻿using Aplication.Interfaces;
+using Aplication.Options;
+using System.Security.Cryptography;
+
+namespace Aplication.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        private readonly PasswordOptions _options;
+        public PasswordService(PasswordOptions passwordOptions)
+        {
+            this._options = passwordOptions;
+        }
+
+        public bool Check(string hash, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Hash(string password)
+        {
+            using (var algorithm = new Rfc2898DeriveBytes(
+               password,
+               _options.SaltSize,
+               _options.Iterations
+               ))
+            {
+                var key = Convert.ToBase64String(algorithm.GetBytes(_options.KeySize));
+
+                var salt = Convert.ToBase64String(algorithm.Salt);
+                return $"{_options.Iterations}.{salt}.{key}";
+            }
+        }
+    }
+}
