c284767aa59f2082115ee7055650834772d6a3db
Julio Rodríguez <julio10489@gmail.com>
2024-02-23 12:59:02 -0500

    Added: Direction Service And Infraestructure. Add Method "AddVoid"

---
+++ Aplication/Services/DirectionService.cs
@@ -0,0 +1,26 @@
+﻿using Aplication.Entities;
+using Aplication.Interfaces;
+
+namespace Aplication.Services
+{
+    public class DirectionService : IDirectionService
+    {
+        private readonly IUnitOfWork<UserDirection> _unitOfWork;
+        public DirectionService(IUnitOfWork<UserDirection> unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+        public async Task<UserDirection> CreateVoid()
+        {
+            var direction = new UserDirection();
+            direction = _unitOfWork.AddressRepo.AddVoid(direction);
+            await _unitOfWork.SaveChangesAsync();
+            return direction;
+        }
+
+        public IEnumerable<UserDirection> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
