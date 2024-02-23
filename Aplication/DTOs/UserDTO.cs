34c22bff4bbd764b7355f96e03dc7ead4d0aa40f
Julio Rodríguez <julio10489@gmail.com>
2024-02-23 12:59:46 -0500

    Changed: User and UserDTO, added Password Property

--- Aplication/DTOs/UserDTO.cs
+++ Aplication/DTOs/UserDTO.cs
@@ -12,7 +12,7 @@
         public string? SecondName { get; set; }
 
         public string Email { get; set; } = null!;
-
+        public string Password { get; set; } = null!;
         public string Phone { get; set; } = null!;
     }
 }
--- Aplication/Entities/User.cs
+++ Aplication/Entities/User.cs
@@ -11,6 +11,7 @@ public partial class User : BaseEntity
     public string? SecondName { get; set; }
 
     public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
     public string Phone { get; set; } = null!;
 
