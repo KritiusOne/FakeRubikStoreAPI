351f74714cf386a6b7b26996b51282f9e1e617be
Julio Rodríguez <julio10489@gmail.com>
2024-02-24 16:28:12 -0500

    Added: Custom Methos for userRepository for find User by Credentials

---
+++ Aplication/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Aplication.Entities;
+
+namespace Aplication.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User GetUserByCredentials(string email);
+    }
+}
