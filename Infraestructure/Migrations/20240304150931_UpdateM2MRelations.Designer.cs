9840f5784dc673d4205c5f44ab2aad3fe698ed49
Julio Rodríguez <julio10489@gmail.com>
2024-03-04 10:22:33 -0500

    added: Migrations part 1

--- Infraestructure/Migrations/20240304150931_UpdateM2MRelations.Designer.cs
+++ Infraestructure/Migrations/20240304150931_UpdateM2MRelations.Designer.cs
@@ -4,6 +4,7 @@ using Infraestructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infraestructure.Migrations
 {
     [DbContext(typeof(FakeRubikStoreContext))]
-    partial class FakeRubikStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240304150931_UpdateM2MRelations")]
+    partial class UpdateM2MRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
