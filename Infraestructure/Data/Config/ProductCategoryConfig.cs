c67a2583dd7f26a847985c2be0ccbedeffc78fa0
Julio Rodríguez <julio10489@gmail.com>
2024-03-04 10:22:19 -0500

    Changed: Relations Many to many of product table

--- Infraestructure/Data/Config/ProductCategoryConfig.cs
+++ Infraestructure/Data/Config/ProductCategoryConfig.cs
@@ -13,12 +13,12 @@ namespace Infraestructure.Data.Config
             entity.Property(e => e.IdProduct).HasColumnName("IdProducto");
             entity.Property(e => e.IdCategory).HasColumnName("IdCategoria");
 
-            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.ProductCategories)
+            entity.HasOne(d => d.CategoryNav).WithMany(p => p.ProductCategories)
                 .HasForeignKey(d => d.IdCategory)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Categorias_Productos_Categorias");
 
-            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.ProductCategories)
+            entity.HasOne(d => d.ProductNav).WithMany(p => p.ProductCategories)
                 .HasForeignKey(d => d.IdProduct)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Categorias_Productos_Productos");
