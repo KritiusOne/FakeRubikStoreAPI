2d098790ee32e7d620470d6c9828f9d698991f50
Julio Rodríguez <julio10489@gmail.com>
2024-03-05 16:52:36 -0500

    Added: Response class

---
+++ API/Response/Response base.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Response
+{
+    public class Response_base<T>
+    {
+        public string Msg { get; set; }
+        public T Response { get; set; }
+        public Response_base(T res, string menssage) {
+            this.Response = res;
+            Msg = menssage;
+        }
+    }
+}
