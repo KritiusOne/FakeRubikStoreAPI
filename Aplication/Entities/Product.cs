8c60d09b5b97347b26a4e8ed6f440cbc1f059bb6
Julio Rodríguez <julio10489@gmail.com>
2024-03-08 13:47:46 -0500

    Changed: string length of image, thumbnail and description is now MAX in DB

--- Aplication/Entities/Product.cs
+++ Aplication/Entities/Product.cs
@@ -11,7 +11,7 @@ public partial class Product : BaseEntity
 
     public int Stock { get; set; }
 
-    public string? Image { get; set; }
+    public string? Image { get; set; } = null!;
 
     public string Description { get; set; } = null!;
 
