412a44b2d5438e881dfaf65d8c794a21917cf587
Julio Rodríguez <julio10489@gmail.com>
2024-03-21 16:28:26 -0500

    Added: Query Filters Class from Products, implementation pending

---
+++ Aplication/QueryFilters/ProductQueryFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace Aplication.QueryFilters
+{
+    public class ProductQueryFilter
+    {
+        public int? ProductID { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+        public string? NameProduct { get; set; }
+        public string? DescriptionProduct { get; set; }
+    }
+}
