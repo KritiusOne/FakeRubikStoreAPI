0f021f39f29a93860da8edf01dbdeceffb0640dc
Julio Rodríguez <julio10489@gmail.com>
2024-03-21 17:38:07 -0500

    Added: New Class from response, for the objetive of give response serialized and paginated

---
+++ API/Response/ResponsePagination.cs
@@ -0,0 +1,18 @@
+﻿using Aplication.CustomEntities;
+
+namespace API.Response
+{
+    public class ResponsePagination<T> : ResponseBase<T>
+    {
+        public MetaData MetaData { get; set; }
+        public int StatusCode { get; set; }
+        public ResponsePagination(T data, 
+            string msg, 
+            int StatusCode, 
+            MetaData meta) : base(data, msg)
+        {
+            this.StatusCode = StatusCode;
+            this.MetaData = meta;
+        }
+    }
+}
