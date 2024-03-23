81dbc31ebb0eebf981283a2d05404e0d87f4c43c
Julio Rodríguez <julio10489@gmail.com>
2024-03-22 22:40:33 -0500

    Added: Service from get Uri of next and previous pages in response

---
+++ Aplication/Services/UriService.cs
@@ -0,0 +1,19 @@
+﻿using Aplication.Interfaces;
+using Aplication.QueryFilters;
+
+namespace Aplication.Services
+{
+    public class UriService : IUriService
+    {
+        private readonly string _url;
+        public UriService(string baseUri)
+        {
+            _url = baseUri;
+        }
+        public Uri GetPostPaginationUri(ProductQueryFilter Filters, string action)
+        {
+            string baseUrl = $"{action}";
+            return new Uri(baseUrl);
+        }
+    }
+}
