4e7e7733eb58eb31c8cd6c550226a2302776292d
Julio Rodríguez <julio10489@gmail.com>
2024-03-22 22:41:18 -0500

    Added: New Interface from UriService

---
+++ Aplication/Interfaces/IUriService.cs
@@ -0,0 +1,9 @@
+﻿using Aplication.QueryFilters;
+
+namespace Aplication.Interfaces
+{
+    public interface IUriService
+    {
+        Uri GetPostPaginationUri(ProductQueryFilter Filters,  string action);
+    }
+}
