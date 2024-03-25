671be13eb867681dce8ca813d906c59b5b7a1b4e
Julio Rodríguez <julio10489@gmail.com>
2024-03-25 18:48:10 -0500

    Added: Sign in Controller

---
+++ API/Controllers/LogIn-SingIn/SignInController.cs
@@ -0,0 +1,68 @@
+﻿using API.Response;
+using Aplication.DTOs;
+using Aplication.Entities;
+using Aplication.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers.LogIn_SingIn
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignInController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserService<User> _userService;
+        private readonly IConfiguration _config;
+        public SignInController(IConfiguration config, IMapper map, IUserService<User> userService)
+        {
+            this._config = config;
+            this._mapper = map;
+            this._userService = userService;            
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddNewUser(UserDTO userDTO)
+        {
+            var user = _mapper.Map<User>(userDTO);
+            var newUser = await _userService.NewUserRegister(user);
+            var newUserDTO = _mapper.Map<UserDTO>(newUser);
+            var token = GenerateToken(newUser);
+            HttpContext.Response.Headers.Add("Auth", token);
+
+            var response = new ResponseBase<UserDTO>(newUserDTO, "The register is succes");
+            return Ok(response);
+        }
+        private string GenerateToken(User user)
+        {
+            //headers
+            var symmetricalSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var singInCredentials = new SigningCredentials(symmetricalSecurityKey, SecurityAlgorithms.HmacSha256);
+            var headers = new JwtHeader(singInCredentials);
+            //Claims
+            var claims = new[]
+            {
+                new Claim("Id", user.Id.ToString()),
+                new Claim("IdRole", user.IdRole.ToString()),
+                new Claim("First_Name", user.Name),
+                new Claim("Last_Name", user.SecondName),
+                new Claim("email", user.Email),
+                new Claim("phone", user.Phone)
+            };
+            //payloads
+            var payloads = new JwtPayload(_config["JWT:Issuer"],
+                _config["JWT:Audience"],
+                claims,
+                DateTime.Now,
+                DateTime.UtcNow.AddHours(10)
+                );
+
+            //Token
+            var Token = new JwtSecurityToken(headers, payloads);
+            return new JwtSecurityTokenHandler().WriteToken(Token);
+        }
+    }
+}
