24d9212bbc4d06343f53aa25fe36d9402ab66b1e
Julio Rodríguez <julio10489@gmail.com>
2024-03-25 18:47:53 -0500

    Changed: interfaces for methods of sign in

--- Aplication/Interfaces/IUserRepository.cs
+++ Aplication/Interfaces/IUserRepository.cs
@@ -5,5 +5,6 @@ namespace Aplication.Interfaces
     public interface IUserRepository : IRepository<User>
     {
         User GetUserByCredentials(string email);
+        Task<User> AddAndReturnUser(User user);
     }
 }
