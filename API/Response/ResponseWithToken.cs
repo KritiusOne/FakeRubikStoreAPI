40e68977a7deb9e37a9b9f93d8ad9d33e03a3fbb
Julio Rodríguez <julio10489@gmail.com>
2024-03-29 11:35:28 -0500

    Added: New class for new type response

---
+++ API/Response/ResponseWithToken.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Response
+{
+    public class ResponseWithToken<T> : ResponseBase<T>
+    {
+        public string Token { get; set; }
+        public ResponseWithToken(string msg, T data, string token) : base(data, msg)
+        {
+            this.Token = token;
+        }
+    }
+}
