042a4c6d76ae8904a73aeaba61ff8f4428ce61cc
Julio Rodríguez <julio10489@gmail.com>
2024-04-10 14:40:15 -0500

    Added: Created ReviewDTO

---
+++ Aplication/DTOs/ReviewDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Aplication.DTOs
+{
+    public class ReviewDTO
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string? Description { get; set; }
+
+        public int Rate { get; set; }
+    }
+}
