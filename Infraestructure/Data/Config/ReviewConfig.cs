e312965942217f1315cc3bf0cfb3419955c1bbda
Julio Rodríguez <julio10489@gmail.com>
2024-04-10 14:39:54 -0500

    Added: new FK from Reviews to Users

--- Infraestructure/Data/Config/ReviewConfig.cs
+++ Infraestructure/Data/Config/ReviewConfig.cs
@@ -13,11 +13,15 @@ namespace Infraestructure.Data.Config
 
             entity.Property(e => e.Rate).HasColumnName("rate");
             entity.Property(e => e.Description).HasColumnName("Descripcion");
-
+            entity.Property(e => e.ProductId).HasColumnName("IdProducto");
+            entity.Property(e => e.UserId).HasColumnName("UsuarioId");
 
             entity.HasOne(e => e.Product).WithMany(x => x.Reviews)
                 .HasForeignKey(e => e.ProductId)
                 .HasConstraintName("FK_Review_Productos");
+            entity.HasOne(e => e.Usuario).WithMany(x => x.Reviews)
+                .HasForeignKey(e => e.UserId)
+                .HasConstraintName("FK_Reviews_Usuarios");
         }
     }
 }
