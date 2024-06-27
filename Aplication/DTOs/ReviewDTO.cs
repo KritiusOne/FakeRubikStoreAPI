7cc21d303464790cf8e57fffcd0c021d446651dd
Julio Rodríguez <julio10489@gmail.com>
2024-06-27 11:58:40 -0500

    Changed: Review DTO

--- Aplication/DTOs/ReviewDTO.cs
+++ Aplication/DTOs/ReviewDTO.cs
@@ -3,7 +3,11 @@
     public class ReviewDTO
     {
         public int Id { get; set; }
+
         public int ProductId { get; set; }
+
+        public int UserId { get; set; }
+
         public string? Description { get; set; }
 
         public int Rate { get; set; }
