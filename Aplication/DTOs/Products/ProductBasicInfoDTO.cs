387f26641ed390210adbe99ba21adfd4ac0c8788
Julio Rodríguez <julio10489@gmail.com>
2024-06-29 15:34:35 -0500

    Changed: New DTOs for better change info

---
+++ Aplication/DTOs/Products/ProductBasicInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Aplication.DTOs.Products
+{
+    public class ProductBasicInfoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public double Price { get; set; }
+
+        public string Thumbnail { get; set; } = null!;
+    }
+}
