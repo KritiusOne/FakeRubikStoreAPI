402fc9760d1367a71ab522ec4bcbfa726cd3db08
Julio Rodríguez <julio10489@gmail.com>
2024-07-02 15:00:23 -0500

    Changed: gouped the DTOs of users entity

--- Aplication/DTOs/Users/CreateUserDTO.cs
+++ Aplication/DTOs/Users/CreateUserDTO.cs
@@ -1,16 +1,9 @@
-﻿namespace Aplication.DTOs
+﻿namespace Aplication.DTOs.Users
 {
-    public class UserDTO
+    public class CreateUserDTO
     {
-        public int Id { get; set; }
-        public int IdRole { get; set; }
-
-        public int IdAddress { get; set; }
-
         public string? Name { get; set; }
-
         public string? SecondName { get; set; }
-
         public string Email { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public string Password { get; set; } = null!;
---
+++ Aplication/DTOs/Users/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Aplication.DTOs.Users
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public int IdRole { get; set; }
+
+        public int IdAddress { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? SecondName { get; set; }
+
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
