6216d1826850573c9d2920876b39164d29c0f00d
Julio Rodríguez <julio10489@gmail.com>
2024-07-02 15:25:42 -0500

    Added: New DTOs with the profile mappers

---
+++ Aplication/DTOs/ProductCategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using Aplication.Entities;
+
+namespace Aplication.DTOs
+{
+    public class ProductCategoryDTO
+    {
+        public int IdCategory { get; set; }
+
+        public int IdProduct { get; set; }
+
+        public CategoryDTO CategoryNav { get; set; } = null!;
+    }
+}
