8f899e97521b102fb1e16c84b8db5b381a11b069
Julio Rodríguez <julio10489@gmail.com>
2024-07-21 17:42:51 -0500

    Added: New service from entity Order, with methods GetAll, Create and GetById

---
+++ Aplication/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using Aplication.CustomEntities;
+using Aplication.Entities;
+using Aplication.QueryFilters;
+
+namespace Aplication.Interfaces
+{
+    public interface IOrderService
+    {
+        PagedList<Order> GetAll(OrderQueryFilters filters);
+        Task<Order> CreateOrder(Order order);
+        Task<Order> GetById(int id);
+    }
+}
