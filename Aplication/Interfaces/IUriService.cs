2d2f710c6c3ae2244939e961ebc8d8c1cabc49ce
Julio Rodríguez <julio10489@gmail.com>
2024-07-21 17:43:47 -0500

    Added: New method for the filters of Order and pagedlist order

--- Aplication/Interfaces/IUriService.cs
+++ Aplication/Interfaces/IUriService.cs
@@ -5,5 +5,6 @@ namespace Aplication.Interfaces
     public interface IUriService
     {
         Uri GetPostPaginationUri(ProductQueryFilter Filters,  string action);
+        public Uri GetPaginationOrder(OrderQueryFilters Filters, string action);
     }
 }
