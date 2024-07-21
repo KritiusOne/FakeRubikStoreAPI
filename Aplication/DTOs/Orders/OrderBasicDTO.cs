e275c968e62553fc1c29f0fcbe5cd12e232949bd
Julio Rodríguez <julio10489@gmail.com>
2024-07-21 17:44:27 -0500

    Added: New DTO's and respective profile method map

---
+++ Aplication/DTOs/Orders/OrderBasicDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Aplication.DTOs.Orders
+{
+    public class OrderBasicDTO
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+
+        public int IdDelivery { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double FinalPrice { get; set; }
+    }
+}
---
+++ Aplication/DTOs/Orders/OrderCompleteInfoDTO.cs
@@ -0,0 +1,15 @@
+﻿using Aplication.DTOs.Users;
+using Aplication.Entities;
+
+namespace Aplication.DTOs.Orders
+{
+    public class OrderCompleteInfoDTO : OrderBasicDTO
+    {
+
+        public DeliveryBasicInfoDTO DeliveryInfo { get; set; } = null!;
+
+        public UserDTO UserInfo { get; set; } = null!;
+
+        public ICollection<OrderProductCompleteInfoDTO> OrderProducts { get; } = new List<OrderProductCompleteInfoDTO>();
+    }
+}
