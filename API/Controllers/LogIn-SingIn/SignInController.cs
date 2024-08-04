7206e497c4eda3fe30026b0ecdd7e37530fd677d
Julio Rodríguez <julio10489@gmail.com>
2024-08-03 19:13:53 -0500

    Changed: Pass to development ambient and delete function no used

--- API/Controllers/LogIn-SingIn/SignInController.cs
+++ API/Controllers/LogIn-SingIn/SignInController.cs
@@ -38,34 +38,5 @@ namespace API.Controllers.LogIn_SingIn
                 "Bearer");
             return Ok(response);
         }
-        private string GenerateToken(User user)
-        {
-            //headers
-            var symmetricalSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
-            var singInCredentials = new SigningCredentials(symmetricalSecurityKey, SecurityAlgorithms.HmacSha256);
-            var headers = new JwtHeader(singInCredentials);
-            //Claims
-            var claims = new[]
-            {
-                new Claim("Id", user.Id.ToString()),
-                new Claim("IdRole", user.IdRole.ToString()),
-                new Claim("IdAddress", user.IdAddress.ToString()),
-                new Claim("First_Name", user.Name),
-                new Claim("Last_Name", user.SecondName),
-                new Claim("email", user.Email),
-                new Claim("phone", user.Phone)
-            };
-            //payloads
-            var payloads = new JwtPayload(_config["JWT:Issuer"],
-                _config["JWT:Audience"],
-                claims,
-                DateTime.Now,
-                DateTime.UtcNow.AddHours(10)
-                );
-
-            //Token
-            var Token = new JwtSecurityToken(headers, payloads);
-            return new JwtSecurityTokenHandler().WriteToken(Token);
-        }
     }
 }
