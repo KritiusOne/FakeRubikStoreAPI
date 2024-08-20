884892cf4bdbd3f9acaeaba49109fdcf74187630
Julio Rodríguez <julio10489@gmail.com>
2024-08-20 18:07:29 -0500

    Added: States Types, delete magic strings/values

---
+++ Aplication/Enums/StatesTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Aplication.Enums
+{
+    public enum StatesTypes
+    {
+        NO_ADMITIDO = 1,
+        EN_PREPARACION,
+        ENVIADO,
+        ENTREGADO,
+        CANCELADO
+    }
+}
