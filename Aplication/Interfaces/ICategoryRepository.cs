c39f2aaf0931f2e8ffb3c6a8ec315be19b356094
Julio Rodríguez <julio10489@gmail.com>
2024-08-21 15:41:55 -0500

    Added: New repository

---
+++ Aplication/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Aplication.Entities;
+
+namespace Aplication.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
---
+++ Infraestructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Aplication.Entities;
+using Aplication.Interfaces;
+using Infraestructure.Data;
+
+namespace Infraestructure.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(FakeRubikStoreContext db) : base(db)
+        {
+            
+        }
+    }
+}
