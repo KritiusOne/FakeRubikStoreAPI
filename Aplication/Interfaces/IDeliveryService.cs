a183717f06633cd9c0c75bba7f9a8a3dc30736ea
Julio Rodríguez <julio10489@gmail.com>
2024-08-20 20:09:13 -0500

    Added: Service, update method

---
+++ Aplication/Interfaces/IDeliveryService.cs
@@ -0,0 +1,8 @@
+﻿namespace Aplication.Interfaces
+{
+    public interface IDeliveryService
+    {
+        Task UpdateState(int newState, int IdDelivery);
+        bool existState(int possibleState);
+    }
+}
---
+++ Aplication/Services/DeliveryService.cs
@@ -0,0 +1,51 @@
+﻿using Aplication.Entities;
+using Aplication.Enums;
+using Aplication.Exceptions;
+using Aplication.Interfaces;
+
+namespace Aplication.Services
+{
+    public class DeliveryService : IDeliveryService
+    {
+        private readonly IUnitOfWork<Delivery> _unitOfWork;
+        public DeliveryService(IUnitOfWork<Delivery> _unitOfWork)
+        {
+            this._unitOfWork = _unitOfWork;
+        }
+
+        public async Task UpdateState(int newState, int IdDelivery)
+        {
+            bool isState = existState(newState);
+            if (isState)
+            {
+                try
+                {
+                    await _unitOfWork.BeginTransactionAsync();
+                    var toUpdated = await _unitOfWork.DeliveryRepo.GetById(IdDelivery);
+                    toUpdated.IdState = newState;
+                    await _unitOfWork.SaveChangesAsync();
+                    _unitOfWork.CommitTransaction();
+                }catch(Exception ex)
+                {
+                    _unitOfWork.RollbackTransaction();
+                    throw new Exception("Unknow element is fall", ex);
+                }
+            }
+            else
+            {
+                throw new BaseException("This state not found");
+            }
+        }
+        public  bool existState(int possibleState)
+        {
+            foreach(StatesTypes state in Enum.GetValues(typeof(StatesTypes)))
+            {
+                if((int)state == possibleState)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
