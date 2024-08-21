9c65e6e96938fa47e81f714aa94e8720977f06d4
Julio Rodríguez <julio10489@gmail.com>
2024-08-20 20:09:51 -0500

    Added: New endpoint, verb PUT

---
+++ API/Controllers/DeliveryController.cs
@@ -0,0 +1,26 @@
+﻿using Aplication.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController : ControllerBase
+    {
+        private readonly IDeliveryService _delivery;
+        private readonly IMapper _map;
+        public DeliveryController(IDeliveryService delivery, IMapper map)
+        {
+            _delivery = delivery;
+            _map = map;
+        }
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateState(int Id, int newState)
+        {
+            await _delivery.UpdateState(newState, Id);
+            return Ok($"El nuevo estado del envío ${Id} es ${newState}");
+        }
+    }
+}
