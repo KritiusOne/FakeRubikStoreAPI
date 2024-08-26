e274afa6f51f6112f24bbc7896101313c6d564fc
Julio Rodríguez <julio10489@gmail.com>
2024-08-25 21:55:15 -0500

    Added: New service, BlobService

---
+++ Aplication/Interfaces/IBlobServices.cs
@@ -0,0 +1,11 @@
+﻿using Aplication.Enums;
+using Azure.Storage.Blobs;
+
+namespace Aplication.Interfaces
+{
+    public interface IBlobServices
+    {
+        BlobContainerClient GetContainer(string containerName, string key);
+        Task<string> UploadBlobAsync(Stream file, AzureBlobTypes BlobTypes, string key);
+    }
+}
---
+++ Aplication/Services/BlobServices.cs
@@ -0,0 +1,36 @@
+﻿using Aplication.Enums;
+using Aplication.Interfaces;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Aplication.Services
+{
+    public class BlobServices : IBlobServices
+    {
+        public BlobContainerClient GetContainer(string containerName, string key)
+        {
+            BlobContainerClient container = new BlobContainerClient(key, containerName);
+            return container;
+        }
+
+        public async Task<string> UploadBlobAsync(Stream file, AzureBlobTypes BlobTypes, string key)
+        {
+            if (file.Length == 0) return null;
+            try
+            {
+                string containerName = Enum.GetName(typeof(AzureBlobTypes), BlobTypes).ToLower();
+                BlobContainerClient container = GetContainer(containerName, key);
+
+                string FileName = Guid.NewGuid().ToString();
+                BlobClient newBlobClient = container.GetBlobClient(FileName);
+
+                await newBlobClient.UploadAsync(file);
+                return FileName;
+
+            }catch(Exception ex)
+            {
+                throw new Exception("Error al subir el blob", ex);
+            }
+        }
+    }
+}
