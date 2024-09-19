ba57e5a80ef616c9d6f1804bcc6a11d68682f129
Julio Rodríguez <julio10489@gmail.com>
2024-09-19 18:53:32 -0500

    Added: Query filters user, from filters in url

---
+++ Aplication/QueryFilters/UserQueryFilters.cs
@@ -0,0 +1,9 @@
+﻿namespace Aplication.QueryFilters
+{
+    public class UserQueryFilters
+    {
+        public int PageSize { get; set; } = 20;
+        public int PageNumber { get; set; } = 1;
+        public int? IdRol { get; set; }
+    }
+}
