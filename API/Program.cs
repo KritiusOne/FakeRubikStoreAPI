4c90d40c8686320b01366bde474c1a26754a915b
Julio Rodríguez <julio10489@gmail.com>
2024-09-23 10:33:37 -0500

    Changed: past to production

--- API/Program.cs
+++ API/Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddSingleton<IUriService>(provider =>
 
 builder.Services.AddDbContext<FakeRubikStoreContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Dev")).LogTo(Console.WriteLine, LogLevel.Information);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Prod")).LogTo(Console.WriteLine, LogLevel.Information);
 });
 
 builder.Services.AddAuthentication(opt =>
