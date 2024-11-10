0743f9f1cc638f42e5436384b36b4b55be272731
Julio Rodríguez <julio10489@gmail.com>
2024-11-10 12:32:22 -0500

    Changed: Change category class for IdsCategories

--- Aplication/QueryFilters/ProductQueryFilter.cs
+++ Aplication/QueryFilters/ProductQueryFilter.cs
@@ -8,7 +8,7 @@ namespace Aplication.QueryFilters
         public int? MaxPrice { get; set; }
         public string? NameProduct { get; set; }
         public string? DescriptionProduct { get; set; }
-        public List<Category>? Categories { get; set; }
+        public List<int>? CategoriesIds { get; set; }
         public int PageSize { get; set; } = 20;
         public int PageNumber { get; set; } = 1;
     }
