24f1f4fba69cc0bf6a56dc3110cae3cdf881a9cf
Julio Rodríguez <julio10489@gmail.com>
2025-02-04 14:49:59 -0500

    Added: New entities

---
+++ Aplication/Entities/Departament.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplication.Entities
+{
+    public class Departament : BaseEntity
+    {
+        public int IdCountry { get; set; }
+        public string Name { get; set; } = null!;
+        public virtual ICollection<City> Cities { get; set; } = new List<City>();
+        public virtual Country Country { get; set; } = null!;
+    }
+}
