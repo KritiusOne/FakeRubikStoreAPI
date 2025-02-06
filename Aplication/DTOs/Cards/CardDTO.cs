5eab3c4bf20ff56a904077a3daa80a009f8ba045
Julio Rodríguez <julio10489@gmail.com>
2025-02-05 20:13:31 -0500

    Added: New DTOs from card and Card type entities

---
+++ Aplication/DTOs/Cards/CardDTO.cs
@@ -0,0 +1,12 @@
+﻿using Aplication.Entities;
+
+namespace Aplication.DTOs.Cards
+{
+    public class CardDTO
+    {
+        public int Id { get; set; }
+        public int IdCardType { get; set; }
+        public string CardNumber { get; set; } = null!;
+        public CardTypeDTO Type { get; set; } = null!;
+    }
+}
