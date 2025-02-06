2a4b1f57cef4b7b7ff5231aad212087c693d454f
Julio Rodríguez <julio10489@gmail.com>
2025-02-05 20:12:54 -0500

    Changed: name property CardNumber

--- Aplication/Entities/Card.cs
+++ Aplication/Entities/Card.cs
@@ -3,7 +3,7 @@
     public class Card : BaseEntity
     {
         public int IdCardType { get; set; }
-        public string NumberCard { get; set; } = null!;
+        public string CardNumber { get; set; } = null!;
         public virtual CardType Type { get; set; } = null!;
         public virtual User User { get; set; } = null!;
     }
