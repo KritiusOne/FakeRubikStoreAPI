37ecccb3620ca374da1f07c2d1c212ef2b6e87d9
Julio Rodríguez <julio10489@gmail.com>
2025-02-13 20:35:08 -0500

    added: config JSON serialize and deserialize

---
+++ Aplication/CustomEntities/LowerCaseUnderscoreNamingPolicy.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Aplication.CustomEntities
+{
+    public class LowerCaseUnderscoreNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            return name.ToLower();
+        }
+    }
+}
